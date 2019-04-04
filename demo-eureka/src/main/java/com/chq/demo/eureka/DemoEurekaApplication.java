61cadccf40c908c7961376a22df998f0831700a9
chenghongqiang <962691576@qq.com>
2019-04-04 16:46:14 +0800

    更新

--- demo-eureka/src/main/java/com/chq/demo/eureka/DemoEurekaApplication.java
+++ demo-eureka/src/main/java/com/chq/demo/eureka/DemoEurekaApplication.java
@@ -2,8 +2,13 @@ package com.chq.demo.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author CHQ
+ */
 @SpringBootApplication
+@EnableEurekaServer
 public class DemoEurekaApplication {
 
     public static void main(String[] args) {
