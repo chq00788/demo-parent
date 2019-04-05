6a54be61a5dc1c38760c3ef7b11d06c9f7d39ff8
chenghongqiang <962691576@qq.com>
2019-04-05 11:05:12 +0800

    增加配置中心

---
+++ demo-config/src/main/java/com/chq/demo/config/ConfigApplication.java
@@ -0,0 +1,18 @@
+package com.chq.demo.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author CHQ
+ * @Description
+ * @date 2019/4/5
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
--- shiro-jwt/src/main/java/com/chq/demo/shiro/jwt/system/controller/UserController.java
+++ shiro-jwt/src/main/java/com/chq/demo/shiro/jwt/system/controller/UserController.java
@@ -2,6 +2,7 @@ package com.chq.demo.shiro.jwt.system.controller;
 
 import com.chq.demo.shiro.jwt.common.entity.ResultMap;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
+    @Value("${server-name}")
+    private String serverName;
+
     @RequestMapping(value = "/getMessage", method = RequestMethod.GET)
     @RequiresPermissions({"system:user:getMessage"})
     public ResultMap getMessage() {
         ResultMap resultMap = new ResultMap();
-        return resultMap.success().message("您拥有用户权限，可以获得该接口的信息！");
+        return resultMap.success().message("您拥有用户权限，可以获得该接口的信息！" + "系统名称:" + serverName);
     }
 }
